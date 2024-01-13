1a2cbab9b91508852770d0e8a41d8aea7640d284
Shubhkiller <shubhamtripathi033@gmail.com>
2024-01-13 12:34:34 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,22 +3,80 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div>
+  <div className="background-buildings sky">
+    <div />
+    <div />
+    <div className="bb1 building-wrap">
+      <div className="bb1a bb1-window" />
+      <div className="bb1b bb1-window" />
+      <div className="bb1c bb1-window" />
+      <div className="bb1d" />
     </div>
+    <div className="bb2">
+      <div className="bb2a" />
+      <div className="bb2b" />
+    </div>
+    <div className="bb3" />
+    <div />
+    <div className="bb4 building-wrap">
+      <div className="bb4a" />
+      <div className="bb4b" />
+      <div className="bb4c window-wrap">
+        <div className="bb4-window" />
+        <div className="bb4-window" />
+        <div className="bb4-window" />
+        <div className="bb4-window" />
+      </div>
+    </div>
+    <div />
+    <div />
+  </div>
+  <div className="foreground-buildings">
+    <div />
+    <div />
+    <div className="fb1 building-wrap">
+      <div className="fb1a" />
+      <div className="fb1b" />
+      <div className="fb1c" />
+    </div>
+    <div className="fb2">
+      <div className="fb2a" />
+      <div className="fb2b window-wrap">
+        <div className="fb2-window" />
+        <div className="fb2-window" />
+        <div className="fb2-window" />
+      </div>
+    </div>
+    <div />
+    <div className="fb3 building-wrap">
+      <div className="fb3a window-wrap">
+        <div className="fb3-window" />
+        <div className="fb3-window" />
+        <div className="fb3-window" />
+      </div>
+      <div className="fb3b" />
+      <div className="fb3a" />
+      <div className="fb3b" />
+    </div>
+    <div className="fb4">
+      <div className="fb4a" />
+      <div className="fb4b">
+        <div className="fb4-window" />
+        <div className="fb4-window" />
+        <div className="fb4-window" />
+        <div className="fb4-window" />
+        <div className="fb4-window" />
+        <div className="fb4-window" />
+      </div>
+    </div>
+    <div className="fb5" />
+    <div className="fb6" />
+    <div />
+    <div />
+  </div>
+</div>
+
   );
 }
 
